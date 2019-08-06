b2135b05a8a8579b592db61d54238a2f1996f558
Davisdavy <davisdvay96@gmail.com>
2019-08-06 14:57:00 +0300

    Add templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,19 @@
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/",(request, response) ->{
+            Map<String,Object>model = new HashMap<String, Object>();
+            model.put("rangers",Ranger.all());
+            model.put("animals","")
+            return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
+    }
 }
