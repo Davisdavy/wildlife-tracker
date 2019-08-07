cbd9fb2d3295bf37e3c538cda741f3c31c9f0b3a
Davisdavy <davisdvay96@gmail.com>
2019-08-07 11:26:53 +0300

    Removed targer folder from repo.

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,7 +3,7 @@ import org.sql2o.Sql2o;
 
 public class DB {
    // postgres://rtufxkpzpyzrrl:42325527e91d6ec584b609cc13599c866825c8b6440ddfdd656df4cddfaee271@ec2-54-204-37-92.compute-1.amazonaws.com:5432/d6pal4uv6rp2t0
+   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets", "davis", "vegas2017");
 
-
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-1-101.compute-1.amazonaws.com:5432/d41or48rdrchmq", "trajcxqbotrgdl", "e41e99b6bb41e8e4b2937b57c48a3d351db73c6b0095b00e1292d46e3df5fe3f");
+    //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-1-101.compute-1.amazonaws.com:5432/d41or48rdrchmq", "trajcxqbotrgdl", "e41e99b6bb41e8e4b2937b57c48a3d351db73c6b0095b00e1292d46e3df5fe3f");
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,8 @@ public class DatabaseRule extends ExternalResource {
 
     @Override
     protected void before() {
-        DB.sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-1-101.compute-1.amazonaws.com:5432/d41or48rdrchmq", "trajcxqbotrgdl", "e41e99b6bb41e8e4b2937b57c48a3d351db73c6b0095b00e1292d46e3df5fe3f");
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", "davis", "vegas2017");
+       // DB.sql2o = new Sql2o("jdbc:postgresql://ec2-54-83-1-101.compute-1.amazonaws.com:5432/d41or48rdrchmq", "trajcxqbotrgdl", "e41e99b6bb41e8e4b2937b57c48a3d351db73c6b0095b00e1292d46e3df5fe3f");
     }
 
     @Override
